80aaadba534f6d86468cdc4b207fff0d1826e475
joelpdurham <joeldur@gmail.com>
2020-01-09 17:00:54 -0800

    can create a user route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const User = require('../lib/models/User');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -16,5 +17,26 @@ describe('app routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('can create a user', async() => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ 
+        email: 'joel@joel.com',
+        userName: 'joel', 
+        password: '1234',
+      })
+      .then(res => {
+        expect(res.header['set-cookie'][0]).toEqual(expect.stringContaining('session='));
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          email: 'joel@joel.com',
+          userName: 'joel',
+          passwordHash: expect.any(String),
+          friendCode: expect.any(String),
+          __v: 0
+        });
+      });
   });
 });
